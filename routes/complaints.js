ded3668409b86794d9bfa49086bd8a40d1d441eb
Rishabh <dplayarrr@gmail.com>
2020-10-22 03:11:44 +0530

    complaint fixed by worker

--- routes/complaints.js
+++ routes/complaints.js
@@ -3,21 +3,41 @@ const route = express.Router();
 const mysql = require("mysql");
 const { ensureAuthenticateds } = require("../config/adminAuth"); //Login Authenticator
 
-route.get('/pending', ensureAuthenticateds, (req,res)=>{
-    let sql = `SELECT * FROM complaint WHERE Checkbox = 0`;
-    db.query(sql, (err,results)=>{
-        res.render('complaints/pending', {
-            user : req.user,
-            results : results
-        })
-    });
 
+var success = [];
+
+route.get('/pending', ensureAuthenticateds, (req,res)=>{
+    let sql = `SELECT * FROM worker`;
+    db.query(sql, (err,results)=>{
+        var workerList = results;
+        let sql = `SELECT * FROM complaint WHERE Checkbox = 0`;
+        db.query(sql, (err,results)=>{
+            res.render('complaints/pending', {
+                user : req.user,
+                results : results,
+                workerList : workerList,
+                success
+            })
+        });
+    });
 });
 
-route.post('/pending', (req,res)=>{
-    let sql = `UPDATE complaint SET Checkbox = 1 WHERE id = "${req.body.id}"`;
-    db.query(sql, (err,results)=>{
-        res.redirect('/adminPanel/complaints/pending');
+
+
+route.post('/pending/workerFixed', (req,res)=>{
+    let sql = `UPDATE complaint SET ? WHERE id = "${req.body.id}"`;
+    var values = {
+        Checkbox : 1,
+        fixedBy : req.body.workerName
+    };
+    db.query(sql, values, (err,results)=>{
+        let sql = `UPDATE worker SET fixedComplaints = fixedComplaints + 1 WHERE Name = "${req.body.workerName}"`;
+        db.query(sql, (err,results)=>{
+            if(!err){
+                req.flash('success_msg', 'Complaint Fixed!');
+                res.redirect('/adminPanel/complaints/pending');
+            }
+        });
     });
 });
 
