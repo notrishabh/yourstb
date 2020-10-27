adc167c8770002c5133217c5df02b1d111b06381
Rishabh <dplayarrr@gmail.com>
2020-10-28 01:27:31 +0530

    Added payment button in unpaid list

--- routes/unpaidList.js
+++ routes/unpaidList.js
@@ -4,6 +4,8 @@ const mysql = require("mysql");
 const {ensureAuthenticateds} = require('../config/adminAuth');    //Login Authenticator
 const Excel = require('exceljs');
 
+var success = [];
+
 
 route.get("/",ensureAuthenticateds,(req,res)=>{
     var d = new Date();
@@ -61,10 +63,91 @@ route.get('/:region_id',ensureAuthenticateds, (req,res)=>{
         res.render('unpaidList/allRegions', {
             user : req.user,
             results : results,
-            region_id : region_id
+            region_id : region_id,
+            success
         });
     });
 });
+
+route.post('/:region_id/pay',ensureAuthenticateds,(req,res)=>{
+    var region_id = req.params.region_id;
+    var amount;
+    var packageOpted;
+    if(req.body.exampleField){
+      amount = req.body.exampleField;
+    }else{
+      amount = req.body.exampleRadios;
+    }
+    if(amount == "153"){
+      packageOpted = "Basic";
+    } else if(amount == "275"){
+      packageOpted = "Silver";
+    }else if(amount == "360"){
+      packageOpted = "Gold";
+    }else if(amount == "454"){
+      packageOpted = "Diamond";
+    }else{
+      packageOpted = "Custom";
+    }
+    db.query(`SELECT * FROM infos WHERE Stb = "${req.body.Stb}"`,(err,results)=>{
+      let sql = `INSERT INTO offline_payment SET ?`;
+      let values = {
+        Name : results[0].Name,
+        Address : results[0].Address,
+        Mobile : results[0].Mobile,
+        Stb : results[0].Stb,
+        Amount : amount,
+        packageOpted : packageOpted
+      };
+      db.query(sql, values, (err,results)=>{
+        if(!err){
+          req.flash('success_msg', 'Paid Successfully!');
+          res.redirect('/adminPanel/unpaidList/' + region_id);
+        }
+      });
+      var d = new Date();
+      var month = new Array();
+      month[0] = "January";
+      month[1] = "February";
+      month[2] = "March";
+      month[3] = "April";
+      month[4] = "May";
+      month[5] = "June";
+      month[6] = "July";
+      month[7] = "August";
+      month[8] = "September";
+      month[9] = "October";
+      month[10] = "November";
+      month[11] = "December";
+                
+      var monthName = month[d.getMonth()];
+      var dateExpiry = new Date();
+      dateExpiry.setDate(dateExpiry.getDate() + 30);
+  
+      let listPay = `UPDATE infos SET ${monthName}="${amount}", datePaid = now(), ?  WHERE Stb = "${results[0].Stb}"`;
+      let listValues = {
+          dateExpiry : dateExpiry
+      };
+      db.query(listPay, listValues, (err,results)=>{
+      });
+  
+      let all_payment = `INSERT INTO all_payment SET ?`;
+      let all_values = {
+          Name : results[0].Name,
+          Address : results[0].Address,
+          Mobile : results[0].Mobile,
+          Stb : results[0].Stb,
+          Amount : amount,
+          Mode : 'Offline',
+          dateExpiry : dateExpiry
+      };
+      db.query(all_payment, all_values, (err,results)=>{
+      });
+  
+  
+    });
+  
+  });
 
 route.get('/:region/download', ensureAuthenticateds,(req,res)=>{
     var region = req.params.region;
