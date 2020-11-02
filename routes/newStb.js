6aa7305fb08cda3534b4b12dc314adfdc9185482
Rishabh <dplayarrr@gmail.com>
2020-11-02 16:02:48 +0530

    new stb insert fixed

--- routes/newStb.js
+++ routes/newStb.js
@@ -19,17 +19,24 @@ route.get('/', ensureAuthenticateds,(req,res)=>{
 });
 
 route.post('/add', ensureAuthenticateds, (req,res)=>{
-    let sql = `INSERT INTO infos SET ?`;
-    let values = {
-        region_id : req.body.region,
-        Name : req.body.name,
-        Address : req.body.address,
-        Mobile : req.body.mobile,
-        Stb : req.body.stb
-    };
-    db.query(sql, values, (err,results)=>{
-        req.flash('success_msg', 'New Connection Added Successfully!');
-        res.redirect('/adminPanel/newStb');
+    let sql = `INSERT INTO infos (region_id, Name, Address, Mobile, Stb) VALUES (${req.body.region}, "${req.body.name}", "${req.body.address}", "${req.body.mobile}", "${req.body.stb}")`;
+    // let values = {
+    //     region_id : req.body.region,
+    //     Name : req.body.name,
+    //     Address : req.body.address,
+    //     Mobile : req.body.mobile,
+    //     Stb : req.body.stb
+    // };
+    db.query(sql, (err,results)=>{
+        if(!err){
+            req.flash('success_msg', 'New Connection Added Successfully!');
+            res.redirect('/adminPanel/newStb');
+        }else{
+            console.log(err);
+            req.flash('error_msg', 'Error Adding New Connection');
+            res.redirect('/adminPanel/newStb');
+    
+        }
     });
 });
 
