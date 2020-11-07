4b0af8877157203283c62292ebb103daf75d391f
Rishabh <dplayarrr@gmail.com>
2020-11-07 19:57:00 +0530

    unsuspend button in suspended list

--- routes/suspendedList.js
+++ routes/suspendedList.js
@@ -2,7 +2,9 @@ const express = require('express');
 const route = express.Router();
 const mysql = require("mysql");
 const {ensureAuthenticateds} = require('../config/adminAuth');    //Login Authenticator
-const Excel = require('exceljs');
+
+var success = [];
+
 
 route.get("/",ensureAuthenticateds,(req,res)=>{
     let sql = `SELECT * FROM infos WHERE suspended = 1`;
@@ -11,9 +13,23 @@ route.get("/",ensureAuthenticateds,(req,res)=>{
         res.render('suspendedList', {
             user : req.user,
             results : results,
+            success
         });
     });
 
+});
+
+route.post('/remove', ensureAuthenticateds, (req,res)=>{
+    let sql = `UPDATE infos SET suspended = 0 WHERE Stb = "${req.body.Stb}"`;
+    db.query(sql, (err,results)=>{
+        if(!err){
+            req.flash('success_msg', 'Stb Unsuspended Successfully!');
+            res.redirect('/adminPanel/suspendedList');
+          }else{
+            req.flash('error_msg', 'Error unsuspending STB');
+            res.redirect('/adminPanel/suspendedList');
+          }    
+    });
 });
 
 
