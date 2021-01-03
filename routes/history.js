29fe8d190600a94221b7c74a968fe81b5b972da3
notrishabh <dplayarrr@gmail.com>
2021-01-03 19:16:57 +0530

    suspended and status in history

--- routes/history.js
+++ routes/history.js
@@ -17,21 +17,39 @@ route.get('/', ensureAuthenticateds, (req,res)=>{
 
 route.post("/",ensureAuthenticateds, (req, res) => {
     var stb = req.body.stb;
-    // let sql = `SELECT * FROM infos WHERE stb = "${stb}"`;
     let sql = `SELECT * FROM all_payment WHERE stb = "${stb}"`;
     db.query(sql, (err, results) => {
       let sql = `SELECT SUM(Amount) AS sum FROM all_payment WHERE stb = "${stb}"`;
-      db.query(sql, (err, sumTotal) => {        
-        res.render("history", {
-        user: req.user,
-        results: results,
-        sumTotal: sumTotal,
-        displayDetails: "block",
-        noResults: "none",
-        success
-      });
+      db.query(sql, (err, sumTotal) => {   
+        let sql = `SELECT suspended, status FROM infos WHERE stb = "${stb}"`;
+        db.query(sql , (err,infos) =>{
+          res.render("history", {
+            user: req.user,
+            results: results,
+            sumTotal: sumTotal,
+            status : infos[0].status,
+            suspended: infos[0].suspended,
+            displayDetails: "block",
+            noResults: "none",
+            success
+          });
+        })     
+        
       })
       
+    });
+  });
+
+  route.post("/unsuspend", ensureAuthenticateds, (req,res)=>{
+    let sql = `UPDATE infos SET suspended = 0 WHERE Stb = "${req.body.stb}"`;
+    db.query(sql, (err,results)=>{
+        if(!err){
+            req.flash('success_msg', 'Stb Unsuspended Successfully!');
+            res.redirect('/adminPanel/history');
+          }else{
+            req.flash('error_msg', 'Error unsuspending STB');
+            res.redirect('/adminPanel/history');
+          }    
     });
   });
 
